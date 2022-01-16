2568230eafd0700201aa8a7b09a7cad0bbd8cbef
Muhammad Yasser <mohyas364@gmail.com>
2022-01-16 05:10:15 +0200

    add animated routes component

---
+++ src/HOC/AnimatedRoutes.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from 'react';
+import { useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+const AnimatedRoutes = ({
+	children,
+	timeout = 400,
+	classNames = 'fade',
+}: PropsWithChildren<{ timeout?: number; classNames?: string }>) => {
+	const location = useLocation();
+	return (
+		<TransitionGroup>
+			<CSSTransition key={location.pathname} classNames={classNames} timeout={timeout}>
+				{children}
+			</CSSTransition>
+		</TransitionGroup>
+	);
+};
+
+export default AnimatedRoutes;
--- src/layouts/main-layout/MainLayout.tsx
+++ src/layouts/main-layout/MainLayout.tsx
@@ -1,10 +1,10 @@
 import { createPortal } from 'react-dom';
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import FloatingButton from '../../components/floating-button/FloatingButton';
 import MainFooter from '../../components/main-footer/MainFooter';
 import MainNavbar from '../../components/main-navbar/MainNavbar';
 import Modal from '../../components/modal/Modal';
+import AnimatedRoutes from '../../HOC/AnimatedRoutes';
 import ArticleDetails from '../../pages/article-details/ArticleDetails';
 import CreateArticle from '../../pages/create-article/CreateArticle';
 import EditArticle from '../../pages/edit-article/EditArticle';
@@ -21,26 +21,26 @@ const MainLayout = () => {
 			<MainNavbar mode={mode} />
 			{mode !== 'Reader' && <FloatingButton mode={mode} />}
 			{createPortal(<Modal mode={mode} />, document.getElementById('modal-container') as HTMLDivElement)}
-			<TransitionGroup>
-				<CSSTransition key={location.pathname} classNames='fade' timeout={400}>
-					<main className='container-fluid'>
-						<Routes location={location}>
-							<Route path={'/'} element={<Navigate to={'/home'} />} />
-							<Route path={'/home'} element={<Home />} />
-							<Route
-								path={'/articles/create'}
-								element={mode === 'Admin' ? <CreateArticle /> : <Navigate to={'/home'} />}
-							/>
-							<Route
-								path={'/articles/:id/edit'}
-								element={mode === 'Admin' ? <EditArticle /> : <Navigate to={'/home'} />}
-							/>
-							<Route path={'/articles/:id'} element={<ArticleDetails />} />
-							<Route path={'*'} element={<Navigate to={'/page-not-found'} />} />
-						</Routes>
-					</main>
-				</CSSTransition>
-			</TransitionGroup>
+
+			<AnimatedRoutes>
+				<main className='container-fluid'>
+					<Routes location={location}>
+						<Route path={'/'} element={<Navigate to={'/home'} />} />
+						<Route path={'/home'} element={<Home />} />
+						<Route
+							path={'/articles/create'}
+							element={mode === 'Admin' ? <CreateArticle /> : <Navigate to={'/home'} />}
+						/>
+						<Route
+							path={'/articles/:id/edit'}
+							element={mode === 'Admin' ? <EditArticle /> : <Navigate to={'/home'} />}
+						/>
+						<Route path={'/articles/:id'} element={<ArticleDetails />} />
+						<Route path={'*'} element={<Navigate to={'/page-not-found'} />} />
+					</Routes>
+				</main>
+			</AnimatedRoutes>
+
 			<MainFooter />
 		</>
 	);
